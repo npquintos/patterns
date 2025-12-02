f1eac14b841b136ae368ec38b0dc5ada3d431784
Noel Quintos <npquintos@gmail.com>
2025-12-02 16:19:33 -0700

    Update statemachineDecorator.py

--- statemachineDecorator.py
+++ statemachineDecorator.py
@@ -3,6 +3,9 @@
     1. Define which functions to call for which state. 'state'
         is an arbitrary string the new state you are in now and
         which function should be called when you reach this state.
+        At the exit point of this function, you have to return the
+        next state and cargo list --> return ['stateA', cargo]. 'cargo'
+        is the container of data to be processed, eg. file pointer.
 
     2. For every function you define in step 1 above,
         decorate them with:
@@ -10,7 +13,11 @@
         @StateMachine.Do('state name')
 
         replacing 'state name' with the name of the state that
-        this function should respond to.
+    this function should respond to. The function name itself is irrelevant.
+    There should be at least one function that return state 'end'. Like this:
+    return ['end', cargo]
+    This is the signal to the state machine that computation is finished and we should
+    exit the state machine loop.
 
     3. Create a StateMachine instance.
 
@@ -19,6 +26,18 @@
         stop once state 'end' is encountered. There should be at
         least one function defined in step1 that will return
         ['end', cargo] or else, the state machine will not stop.
+
+    TIP: make 'cargo' a class so that you could easily save a value to it for consumption
+         in the state procedure like below.
+         
+         class G:
+            pass
+         cargo = G()
+         cargo.fin = open('data.txt', 'r')
+         cargo.count = 0
+         cargo.accumulator = []
+         sm = StateMachine()
+         sm.run('start', cargo) # 'start' is a state with corresponding decorated function
 '''
 class StateMachine:
     proc_given_state = {}
@@ -49,7 +68,7 @@ if __name__ == '__main__':
         print(f"second_function called with cargo {cargo}, now calling end")
         cargo = 'CCC'
         return ['end', cargo] ######### VERY IMPORTANT to have at least one
-                              ######### returnung ['end', cargo]
+                              ######### returning ['end', cargo]
 
     @StateMachine.Do('third')
     def third_function(cargo): # state 'third'
@@ -65,4 +84,5 @@ if __name__ == '__main__':
     ######## Step 4 ########
     cargo = '000'
     sm.run('first', cargo)
+
 
